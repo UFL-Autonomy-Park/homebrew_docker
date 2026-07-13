de5f795b2492da7393328694311962219395327b
uflautonomypark <uflautonomypark@gmail.com>
2026-07-13 12:02:47 -0400

    fix: discovery server xml file

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -10,15 +10,30 @@ export ROS_LOCALHOST_ONLY="${ROS_LOCALHOST_ONLY:-0}"
 export RMW_IMPLEMENTATION="${RMW_IMPLEMENTATION:-rmw_fastrtps_cpp}"
 
 # Check if discovery server
+unset ROS_DISCOVERY_SERVER
+unset ROS_SUPER_CLIENT
+
 if [[ "${USE_DISCOVERY_SERVER:-false}" == "true" ]]; then
-    if [[ -z "${DISCOVERY_SERVER_ADDRESS:-}" ]]; then
-        echo "USE_DISCOVERY_SERVER=true, but DISCOVERY_SERVER_ADDRESS is empty." >&2
+    if [[ ! -r "${FASTRTPS_PROFILE_PATH}" ]]; then
+        echo "Fast DDS profile not found: ${FASTRTPS_PROFILE_PATH}" >&2
         exit 1
     fi
 
-    export ROS_DISCOVERY_SERVER="${DISCOVERY_SERVER_ADDRESS}"
+    export FASTRTPS_DEFAULT_PROFILES_FILE="${FASTRTPS_PROFILE_PATH}"
 else
-    unset ROS_DISCOVERY_SERVER
+    unset FASTRTPS_DEFAULT_PROFILES_FILE
+fi
+
+echo "ROS_DISTRO=${ROS_DISTRO}"
+echo "ROS_DOMAIN_ID=${ROS_DOMAIN_ID}"
+echo "ROS_LOCALHOST_ONLY=${ROS_LOCALHOST_ONLY}"
+echo "RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION}"
+
+if [[ -n "${FASTRTPS_DEFAULT_PROFILES_FILE:-}" ]]; then
+    echo "Fast DDS Discovery Server profile enabled:"
+    echo "  ${FASTRTPS_DEFAULT_PROFILES_FILE}"
+else
+    echo "Fast DDS Discovery Server profile disabled"
 fi
 
 exec "$@"
